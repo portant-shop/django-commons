86f2796876885c899cbd57bc86115eccf3620ba5
Vedran Vojvoda <vedran@pinkdroids.com>
2022-03-05 03:03:48 +0100

    Fix dependancy problem

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
-    version="1.0.1",
+    version="1.0.2",
     license="MIT",
     url=URL,
     packages=find_packages(),
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "Django>=3.0",
-        "Django>=54.1",
+        "weasyprint>=54.1",
         "django-imagekit>=4.1.0",
         "django-ckeditor>=6.2.0"
     ],
